8ff154adee65ec14ecefd2c475a7699028bf2ac8
Prakhar Agrawal <prakhar_agrawal@epam.com>
2024-01-15 17:18:11 +0530

    rendered UI using graphql

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      userId
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
 
 function App() {
+  const { data, loading } = useQuery(query);
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          {data.getTodos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo?.user?.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
